d20d050bd5589acbfc0ceaf3f0526ac2489c6b92
VahidRasekhi <74292635+VahidRasekhi@users.noreply.github.com>
2026-06-06 17:16:16 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,37 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            'سلام از گیت هاب!',
-            style: TextStyle(fontSize: 40),
-          ),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter App'),
+      ),
+      body: const Center(
+        child: Text(
+          'Hello from GitHub!',
+          style: TextStyle(fontSize: 40),
         ),
       ),
     );
